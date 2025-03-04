b1a46c81918afca49ef4252e1d83d64c66197850
Anis Fetouab <91206298+AnisFetouab@users.noreply.github.com>
2025-03-04 23:14:51 +0100

    Update Project.tsx

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -31,11 +31,13 @@ const Project = () => {
             
             <div className="animate-on-scroll animate-slide-in-right">
               <div className="relative rounded-2xl overflow-hidden shadow-lg">
-                <img
-                  src="https://www.dropbox.com/s/qfi03jgp308hmtx/d46a9aaf-94cf-41c2-9749-9ef66ed8e892.jpg?st=60r9tvb2&dl=0"
+                <img src="https://www.dropbox.com/s/qfi03jgp308hmtx/d46a9aaf-94cf-41c2-9749-9ef66ed8e892.jpg?st=60r9tvb2&dl=0"
                   alt="EcoHaven London Project"
                   className="w-full h-auto"
-                />
+                  />
+              </div>
+            </div>
+
               </div>
             </div>
           </div>
